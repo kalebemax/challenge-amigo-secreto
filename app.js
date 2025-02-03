774f522b1c323b2270466bcf312a25c1a87d7aee
kalebemax <169088614+kalebemax@users.noreply.github.com>
2025-02-03 10:47:35 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+let amigos = [];
+
 function adicionarAmigo() {
-  let nomesDigitados = document.querySelector('input').value;
+  let nomesDigitados = document.querySelector("input").value;
+  amigos.push(nomesDigitados);
+  limparCampo();
+  console.log(amigos);
+
   if (nomesDigitados == 0) {
-    alert('Por favor, digite um nome válido.')
+    alert("Por favor, digite um nome válido.");
+    amigos = [];
   }
+
+  let listaAmigos = document.querySelector("ul");
+  listaAmigos.innerHTML = amigos;
+}
+
+function limparCampo() {
+  nomesDigitados = document.querySelector("input");
+  nomesDigitados.value = "";
+}
+
+function atualizarLista() {
+  let lista = document.querySelector("ul");
+  lista.innerHTML = "";
+  amigos = [];
+}
+
+function sortearAmigo() {
+  let res = document.getElementById("resultado");
+  let nomeSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  console.log(nomeSorteado);
+  
+  if (amigos.length == 0) {
+    alert("Por favor, Adicione nomes ao campo para realizar o sorteio.");
+  } else if (amigos.length == 1) {
+    alert(
+      "Adicione pelo menos dois nomes à lista para o sorteio ser realizado."
+    );
+  } else {
+    res.innerHTML = `${nomeSorteado}`;
+    atualizarLista();
+  }
+  
 }
